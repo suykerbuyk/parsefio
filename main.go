9bcf134fadc02269dbe766da355f2872b9850b58
John-S <git@suykerbuyk.org>
2019-12-23 12:11:23 -0700

    Initial commit of (working) prototype code
    The only thing of value is the go struct definition of FIO json output
    and a roundtrip import/export of the data from a sample test file.

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/wI2L/jettison"
+	"io/ioutil"
+	"os"
+	"reflect"
+)
+
+func describe(f interface{}) {
+	val := reflect.TypeOf(f).Elem()
+	for i := 0; i < val.NumField(); i++ {
+		typeF := val.Field(i)
+		fieldName := typeF.Name
+		jsonTag := typeF.Tag.Get("json")
+		fmt.Printf("Field: %s  jsonTag: %s\n", fieldName, jsonTag)
+		fmt.Println(typeF)
+	}
+}
+
+func main() {
+	textPtr := flag.String("text", "my_text", "Something texty")
+	flag.Parse()
+	fmt.Println("test flag: %s", *textPtr)
+	fmt.Println(flag.Args())
+	jsonFile, err := os.Open("librbd-lr02u27-J24-write-32k.fio.json")
+	if err != nil {
+		panic(err)
+	}
+	defer jsonFile.Close()
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		panic(err)
+	}
+	var result FioData
+	json.Unmarshal([]byte(byteValue), &result)
+	describe(&result)
+	fmt.Println("===============================")
+	describe(&result.GlobalOptions)
+	fmt.Println("===============================")
+	fmt.Printf("FioVersion %s\n", result.FioVersion)
+	fmt.Printf("GlobalOptions.IoEngine %s\n", result.GlobalOptions.Ioengine)
+	fmt.Printf("Job elements %d\n", len(result.Jobs))
+	for idx, job := range result.Jobs {
+		fmt.Printf("Idx: %d Jobname:%s\n", idx, job.Jobname)
+		describe(&job.Read)
+	}
+	fmt.Println("===============================")
+	jsonOut, err := jettison.Marshal(result)
+	if err != nil {
+		panic(err)
+	}
+	os.Stdout.Write(jsonOut)
+}
